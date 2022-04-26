ec101c8f8abadf2d05cc5056eaca5e55ef7521c9
Vinicius <viniciusmouraaragao@hotmail.com>
2022-04-26 10:47:53 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var meuNome = "vinicius";
+var meuArray = meuNome.split('')
+var letrasRepetidas = []
+
+meuArray.forEach(letra => {
+    for (var i = 0; i < meuNome.length; i++) {
+        if(i >= 0 && letra == meuArray[i] && i != meuArray.indexOf(meuArray[i])) {
+            console.log(`'${letra}' tem letra repetida`)
+            letrasRepetidas.push(letra)
+            break
+        }
+    }
+})
+
+console.log(letrasRepetidas)
