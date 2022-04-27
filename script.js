9ba908db25c569c7ecb0108ca25cbcc79b826509
Vinicius Aragão <55242418+bsqqq@users.noreply.github.com>
2022-04-27 18:00:24 -0300

    Update script.js
    desconsiderando espaço entre os nomes

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var letrasRepetidas = []
 meuArray.forEach(letra => {
     for (var i = 0; i < meuNome.length; i++) {
         if(i >= 0 && letra == meuArray[i] && i != meuArray.indexOf(meuArray[i])) {
+            if(letra == " ") {
+                break
+            }
             console.log(`'${letra}' tem letra repetida`)
             letrasRepetidas.push(letra)
             break
